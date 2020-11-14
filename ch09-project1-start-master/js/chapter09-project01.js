16d64fea8d4f419ad934f1be32cca1ed8a577954
anilmathew1994 <anilmathew815@gmail.com>
2020-11-13 20:25:02 -0500

    Commiting the preventDefault function

--- ch09-project1-start-master/js/chapter09-project01.js
+++ ch09-project1-start-master/js/chapter09-project01.js
@@ -21,30 +21,63 @@ function myFuncBlur1(){
     var mus=document.getElementById("museum");
     mus.classList.remove("highlightable");
     }
-
+    
     function myFuncFocus1(){
-        var title=document.getElementById("title");
-        title.classList.add("highlightable");
-        title.classList.remove("error");
-        }
-        function myFuncFocus2(){
-        var des=document.getElementById("description");
-        des.classList.add("highlightable");
-        des.classList.remove("error");
-        }
-        function myFuncFocus3(){
-        var med=document.getElementById("medium");
-        med.classList.add("highlightable");
-        med.classList.remove("error");
-        }
-        function myFuncFocus4(){
-        var yr=document.getElementById("year");
-        yr.classList.add("highlightable");
-        yr.classList.remove("error");
-        }
-        function myFuncFocus5(){
-        var mus=document.getElementById("museum");
-        mus.classList.add("highlightable");
-        mus.classList.remove("error");
-        } 
-   
+    var title=document.getElementById("title");
+    title.classList.add("highlightable");
+    title.classList.remove("error");
+    }
+    function myFuncFocus2(){
+    var des=document.getElementById("description");
+    des.classList.add("highlightable");
+    des.classList.remove("error");
+    }
+    function myFuncFocus3(){
+    var med=document.getElementById("medium");
+    med.classList.add("highlightable");
+    med.classList.remove("error");
+    }
+    function myFuncFocus4(){
+    var yr=document.getElementById("year");
+    yr.classList.add("highlightable");
+    yr.classList.remove("error");
+    }
+    function myFuncFocus5(){
+    var mus=document.getElementById("museum");
+    mus.classList.add("highlightable");
+    mus.classList.remove("error");
+    }
+    
+    function preventDefault(){
+    var title=document.getElementById("title");
+    var des=document.getElementById("description");
+    var med=document.getElementById("medium");
+    var yr=document.getElementById("year");
+    var mus=document.getElementById("museum");
+    
+    if(title.value==""){
+    title.classList.add("error");
+    }
+    else{
+    title.classList.remove("error");
+    }
+    
+    
+    if(des.value==""){
+    des.classList.add("error");
+    }
+    else{
+    des.classList.remove("error");
+    }
+    
+    
+    if(yr.value==""){
+    yr.classList.add("error");
+    }
+    else{
+    yr.classList.remove("error");
+    }
+    
+    
+    return(false);
+    }
