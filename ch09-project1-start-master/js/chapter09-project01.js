8c0f50c0ec65cfe572f233fdfa30e814be92f46c
ageorge11 <ageorg11@confederationcollege.ca>
2020-11-13 20:04:15 -0500

    Commiting HTML and Javascript Code for function onBlur

---
+++ ch09-project1-start-master/js/chapter09-project01.js
@@ -0,0 +1,26 @@
+
+/* add code here  */
+
+function myFuncBlur1(){
+    var title=document.getElementById("title");
+    title.classList.remove("highlightable");
+    }
+    function myFuncBlur2(){
+    var des=document.getElementById("description");
+    des.classList.remove("highlightable");
+    }
+    function myFuncBlur3(){
+    var med=document.getElementById("medium");
+    med.classList.remove("highlightable");
+    }
+    function myFuncBlur4(){
+    var yr=document.getElementById("year");
+    yr.classList.remove("highlightable");
+    }
+    function myFuncBlur5(){
+    var mus=document.getElementById("museum");
+    mus.classList.remove("highlightable");
+    }
+
+    
+   
